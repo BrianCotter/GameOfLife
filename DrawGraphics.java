f4ed3dbc02d2360294085735e27fe237727f328e
BrianCotter <72478052+BrianCotter@users.noreply.github.com>
2020-10-09 15:29:31 -0400

    Update DrawGraphics.java
    Fixed indentation

--- DrawGraphics.java
+++ DrawGraphics.java
@@ -8,12 +8,12 @@ import java.awt.Graphics;
 public class DrawGraphics {
     Grid grid; 
     
-	/* Initializes this class for drawing. */
+    // Initializes this class for drawing
     public DrawGraphics() {
         grid = new Grid();
     }
 	
-    /* Draw the contents of the window on surface. Called 20 times per second. */
+    // Draw the contents of the window on surface. Called 20 times per second 
     public void draw(Graphics g) {
         grid.draw(g);
     }
